d86ad49157911628fe7996e59840310117df3a66
rubens.rangel <rubens.rangel@b2wdigital.com>
2022-05-25 16:13:07 -0300

    first commit

---
+++ src/test/java/br/rubensrangel/KafkaEcommerce/KafkaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rubensrangel.KafkaEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
